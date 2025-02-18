0a0ea82ffccb484afdfb3b8e71e6cef54ad3690e
Sam <swilliamson_0907@outlook.com>
2025-02-18 15:51:14 +0200

    added postgre db and connected

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,29 @@
+require('dotenv').config();
+
 const express = require('express')
 const cors = require('cors')
+const { Sequelize } = require('sequelize');
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: console.log, // Logs SQL queries (useful for debugging)
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log('✅ Connected to PostgreSQL successfully!'))
+  .catch((err) => console.error('❌ PostgreSQL connection error:', err));
+
+
 app.get('/', (req, res) => {
   res.send('TriSwift Backend is Running!')
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

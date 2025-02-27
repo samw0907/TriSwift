71ca9a06ee47dfb4233d58eb4c8c9e50bd99ee67
Sam <swilliamson_0907@outlook.com>
2025-02-27 08:22:30 +0200

    set up front end dashboard

--- frontend/src/graphql/queries.ts
+++ frontend/src/graphql/queries.ts
@@ -1,1 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const GET_WORKOUTS = gql`
+  query GetWorkouts {
+    workouts {
+      id
+      session_type
+      date
+      total_duration
+      total_distance
+      weather_temp
+      weather_humidity
+      weather_wind_speed
+    }
+  }
+`;
+
 export {};

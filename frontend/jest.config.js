080ab9777e5ce20a8be72def71c5485402de181e
Sam <swilliamson_0907@outlook.com>
2025-03-12 16:56:47 +0200

    tests still not running correctly

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
     moduleNameMapper: {
-      "\\.(css|scss|sass)$": "identity-obj-proxy"
+      "\\.(css|scss|sass)$": "identity-obj-proxy",
+      "^react-router-dom$": "<rootDir>/node_modules/react-router-dom"
     },
-  };
-  
+    transform: {
+      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+    },
+    roots: ["<rootDir>/src"]
+};
--- frontend/src/components/__tests__/Navbar.test.tsx
+++ frontend/src/components/__tests__/Navbar.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../Navbar";
 
 test("renders all navigation links", () => {
   render(

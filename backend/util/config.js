f99a7d6151e44912c05cc8bc5a215e819d115eea
Sam <swilliamson_0907@outlook.com>
2025-03-21 08:30:58 +0200

    backend deployed succesfully

--- backend/util/config.js
+++ backend/util/config.js
@@ -1,4 +1,12 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+console.log('Loaded config:', {
+  NODE_ENV: process.env.NODE_ENV,
+  DATABASE_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+});
 
 module.exports = {
   PORT: process.env.PORT || 3001,

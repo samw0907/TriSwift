4a34cdb4085cd6561381f01edcecfa7503c09ab6
Sam <swilliamson_0907@outlook.com>
2025-03-26 09:31:38 +0200

    playwright config change

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   webServer: {
     command: 'npm start',
     port: 3000,
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
     timeout: 120 * 1000,
   },
 });

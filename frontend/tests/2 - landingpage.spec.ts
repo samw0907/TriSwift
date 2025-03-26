121812cfdb87e73b105b86659f2d065a018c79a1
Sam <swilliamson_0907@outlook.com>
2025-03-26 15:22:47 +0200

    adding deploy to ci

--- frontend/tests/2 - landingpage.spec.ts
+++ frontend/tests/2 - landingpage.spec.ts
@@ -7,7 +7,7 @@ test('Landing page should load for logged-out users', async ({ page }) => {
 
   await expect(page.locator('h1')).toHaveText('Welcome to TriSwift');
 
-  await expect(page.locator('p').first()).toContainText('Your all-in-one training companion for swimming, cycling, and running.');
+  await expect(page.locator('p').first()).toContainText('The ultimate fitness tracking app for triathletes.');
 
   await expect(page.locator('button', { hasText: 'Login' })).toBeVisible();
   await expect(page.locator('button', { hasText: 'Sign Up' })).toBeVisible();
